d852cb762131d90f720b72d1651b34ca79eae36b
ichbtrv <ivanchebotarev215@gmail.com>
2022-01-09 10:15:13 -0800

    Added "Sign Up" header to UserCreateFormContainer

--- components/form/UserCreateFormContainer.tsx
+++ components/form/UserCreateFormContainer.tsx
@@ -1,5 +1,16 @@
 const UserCreateFormContainer = () => {
-  return <div>Container</div>
+  return (
+    <div
+      className="p-2 flex items-center flex-col mb-8 
+    "
+    >
+      <div className=" rounded-xl">
+        <h1 className="text-6xl  mt-12 mb-10 px-12">
+          Sign Up for <span className="text-green-400">Rewards</span>
+        </h1>
+      </div>{' '}
+    </div>
+  )
 }
 
 export default UserCreateFormContainer
