f847e5faca375c7b780615de7df9941a5da6ec7e
ichbtrv <ivanchebotarev215@gmail.com>
2022-01-09 11:07:53 -0800

    Added FormValues interface

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,3 +1,11 @@
 export interface LoginCardProps {
   children: React.ReactNode
 }
+
+export interface FormValues {
+  name: string
+  email: string
+  password: string
+  occupation: string
+  state: string
+}
