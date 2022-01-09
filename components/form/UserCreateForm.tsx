e173c8b056fb3c6b1fd2ced6a94abe45fef8329e
ichbtrv <ivanchebotarev215@gmail.com>
2022-01-09 10:25:05 -0800

    added useGetForm hook and useForm(reacthookform)

--- components/form/UserCreateForm.tsx
+++ components/form/UserCreateForm.tsx
@@ -1,4 +1,15 @@
+import { useForm } from 'react-hook-form'
+import { useGetForm } from './hooks/useGetForm'
+
 const UserCreateForm = () => {
+  const { data } = useGetForm()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
   return <div>UserCreateForm</div>
 }
 
