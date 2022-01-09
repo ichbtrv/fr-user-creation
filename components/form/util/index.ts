c58cfe5fd52e4b0e435401783916057edfcd6f97
ichbtrv <ivanchebotarev215@gmail.com>
2022-01-09 11:29:05 -0800

    Removed console.log

--- components/form/util/index.ts
+++ components/form/util/index.ts
@@ -2,7 +2,6 @@ import axios from 'axios'
 import { FormValues } from 'interfaces'
 
 export const postForm = async (submittedFormData: FormValues) => {
-  console.log(submittedFormData)
   const data = await axios.post(
     `https://frontend-take-home.fetchrewards.com/form`,
     submittedFormData
