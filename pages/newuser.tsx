22813019f8829c13d5ff35c052a6d56527cbbb06
ichbtrv <ivanchebotarev215@gmail.com>
2022-01-09 11:02:43 -0800

    Created newuser page

---
+++ pages/newuser.tsx
@@ -0,0 +1,9 @@
+const NewUserPage = () => {
+  return (
+    <div className="flex flex-col items-center min-h-screen justify-center text-2xl text-green-400">
+      Success! <div className="text-black">Welcome!</div>
+    </div>
+  )
+}
+
+export default NewUserPage
